ed52ce70da3d2b301ea311103e9f201ca2305231
Rizwan Irfan X <rizwanirfanx@gmail.com>
2023-02-25 23:05:25 +0500

    Tested & Integrated Tailwind into the project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,19 +2,23 @@
 
 <head>
     <title>App Name - @yield('title')</title>
+    @vite('resources/css/app.css')
 </head>
 
 <body>
     <nav>
-	<div class="logo_container">
+        <div class="logo_container">
 
-	</div>
-	<div class="links">
-		<a>Home</a>
-		<a>How to Cast Vote</a>
-		<a>FAQs</a>
-		<a>Contact Us</a>
-	</div>
+        </div>
+        <div class="links">
+            <a>Home</a>
+            <a>How to Cast Vote</a>
+            <a>FAQs</a>
+            <a>Contact Us</a>
+        </div>
+        <h1 class="text-3xl font-bold underline text-slate-500">
+            Hello world!
+        </h1>
     </nav>
     <div class="container">
         @yield('content')
