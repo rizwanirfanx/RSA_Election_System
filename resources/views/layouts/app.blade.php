2bccb259d7a3942390b4e5a9791ad3af122be0b1
Rizwan Irfan X <rizwanirfanx@gmail.com>
2023-02-28 12:17:56 +0500

    Laid Structure of Welcome & Login Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,22 +6,25 @@
 </head>
 
 <body>
-    <nav class="flex items-center justify-between w-11/12 m-auto bg-green-900 ">
+    <nav class="flex items-center justify-between w-11/12 m-auto ">
         <div class="logo_container">
             <img width="100px" src="{{ Vite::asset('resources/images/ecp_logo.png') }}">
 
 
         </div>
         <div class="links">
-            <a class="p-2">Home</a>
+            <a class="p-2 hover:to-green-100">Home</a>
             <a class="p-2">How to Cast Vote</a>
             <a class="p-2">FAQs</a>
             <a class="p-2">Contact Us</a>
         </div>
     </nav>
-    <div class="container">
+    <div class="">
         @yield('content')
     </div>
 </body>
+<footer class="fixed bottom-0 text-center">
+	<div class="text-center">Election System Developed by RSA Team</div>
+</footer>
 
 </html>
