2ea41b289fdc300a04308c380c139add877b3976
saad <saadwisdom452@gmail.com>
2023-06-23 00:35:53 +0500

    Added Emmad Image

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -1,2 +1,2 @@
+<img src="https://pps.whatsapp.net/v/t61.24694-24/256024995_330833945622355_2114547163785505603_n.jpg?ccb=11-4&oh=01_AdRVhdsTLTLzZ9rmFVz7opZ34Ssyq_r8Ng7P_y-YxElTHA&oe=64A19BD7"/>
 
-<h1>Chokragaan are the best</h1>
