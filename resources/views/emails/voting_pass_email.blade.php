57e8dbdfd738c3fccf19bd83431dcdb33c70422c
superuserx <rizwanirfanx@gmail.com>
2023-06-23 01:45:25 +0500

    Added Email Template v1

--- resources/views/emails/voting_pass_email.blade.php
+++ resources/views/emails/voting_pass_email.blade.php
@@ -1,1 +1,33 @@
-<h1>This is your Voting Pass {{ $voting_pass }}</h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>RSA Voting Pass</title>
+</head>
+<body>
+  <div style="text-align: center;">
+    <!-- Your Company Logo -->
+    <img src="{{asset('images/ecp_logo.png')}}" alt="ECP Logo" style="max-width: 200px; margin-bottom: 20px;">
+  </div>
+
+  <div style="text-align: center;">
+    <p>Here is your Voting Pass, Don't Share with Others, keep it Secret 😶</p>
+		<p><b>{{$voting_pass}}</b></p>
+
+    <button id="copyButton" onclick="copyToClipboard()">Copy To Clipboard</button>
+  </div>
+
+  <div style="text-align: center;">
+    <!-- Image Section -->
+    <img src="asset('images/header.png" alt="Image" style="max-width: 400px; margin-top: 20px;">
+  </div>
+
+  <script>
+    function copyToClipboard() {
+      var textToCopy = "[Text you want to copy]";
+      navigator.clipboard.writeText(textToCopy);
+      alert("Text copied to clipboard!");
+    }
+  </script>
+</body>
+</html>
+
